a6ebcefb44996df625534f5dbc1ce8998e4d5636
Michael <89100298+MichaelRogerss@users.noreply.github.com>
2023-04-07 01:11:02 -0400

    added more animations

--- main.js
+++ main.js
@@ -4,31 +4,59 @@ const slides = document.querySelectorAll('.slide');
 
 const selectors = document.querySelectorAll('.selector');
 
+var activeSlide = 0;
+document.getElementById("im2").classList.add("show")
 Array.from(selectors).forEach((selector, index) => {
   selector.addEventListener('click', () => {
-    selectors.forEach(selector2 => {
-        selector2.classList.remove("isactive");
-    })
-    slides.forEach(slide => {
-        slide.classList.remove('activeslide');
-    })
-    selector.classList.add("isactive");
-    const slideTo = slides[index];
-    slides[index].classList.add('activeslide');
 
+    if(index != activeSlide){
+        activeSlide = index;
+        selectors.forEach(selector2 => {
+            selector2.classList.remove("isactive");
+        })
+        slides.forEach(slide => {
+            slide.classList.remove('activeslide');
+        })
+        selector.classList.add("isactive");
+        const slideTo = slides[index];
+        slides[index].classList.add('activeslide');
 
-    if(index == 0){
-        document.getElementsByClassName("navmiddle")[0].classList.remove("dark")
-        document.getElementsByClassName("shopCart")[0].classList.remove("dark")
-        document.getElementsByClassName("sliderFrame")[0].style.backgroundColor = 'var(--orange)'
-    }else if(index == 1){   
-        document.getElementsByClassName("navmiddle")[0].classList.add("dark")
-        document.getElementsByClassName("shopCart")[0].classList.add("dark")
-        document.getElementsByClassName("sliderFrame")[0].style.backgroundColor = 'var(--pina)'
+        document.getElementById("im2").classList.remove("show")
+        if(index == 0){
+            document.getElementById("im2").classList.add("show")
+            document.getElementsByClassName("navmiddle")[0].classList.remove("dark")
+            document.getElementsByClassName("shopCart")[0].classList.remove("dark")
+            document.getElementsByClassName("sliderFrame")[0].style.backgroundColor = 'var(--orange)'
+        }else if(index == 1){   
+            document.getElementsByClassName("navmiddle")[0].classList.add("dark")
+            document.getElementsByClassName("shopCart")[0].classList.add("dark")
+            document.getElementsByClassName("sliderFrame")[0].style.backgroundColor = 'var(--pina)'
+        }else if(index == 2){
+            document.getElementsByClassName("navmiddle")[0].classList.remove("dark")
+            document.getElementsByClassName("shopCart")[0].classList.remove("dark")
+            document.getElementsByClassName("sliderFrame")[0].style.backgroundColor = 'var(--mango)'
+        }
+        
+        const elements = document.querySelectorAll('.leaf');
+
+        
+        elements.forEach(element => {
+        
+        const currentRotation = getComputedStyle(element).getPropertyValue('transform');
+
+        
+        const match = currentRotation.match(/matrix\((.*)\)/);
+        const matrix = match ? match[1].split(',') : [0, 0, 0, 0, 0, 0];
+        const angle = Math.round(Math.atan2(matrix[1], matrix[0]) * (180 / Math.PI));
+
+        
+        element.style.transform = `rotate(${angle + 80}deg)`;
+        });
+        slider.scroll({
+            left: slideTo.offsetLeft,
+            behavior: 'smooth'
+        });
     }
-    slider.scroll({
-        left: slideTo.offsetLeft,
-        behavior: 'smooth'
-      });
+    
   });
 });
